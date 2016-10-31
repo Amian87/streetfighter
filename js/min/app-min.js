93b279d3816e9ae7af994ccce8bcda1ae0a4ee55
Abdulla Mian <miana@MianAMac01.clayco.net>
2016-10-31 18:57:11 -0500

    added ryu gif

---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".ryu-standing").prepend('<img src="/images/ryu-ready-position.gif" />'),$(".ryu-standing").click(function(){$(".ryu-standing").html("<img src=/images/ryu-standing-still.png>")})});
