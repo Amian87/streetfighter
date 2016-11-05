2663a0b5e039f068454a9a53b954bad13cb3c26b
Abdulla Mian <miana@MianAMac01.clayco.net>
2016-11-05 14:49:21 -0500

    added gif

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-	$('.ryu-standing').prepend('<img src="/images/ryu-ready-position.gif" />');
+	$('.ryu-standing').attr('src', '/images/ryu-ready-position.gif' />);
+	
 	$('.ryu-standing').click(function(){
-		$('.ryu-standing').html('<img src=/images/ryu-standing-still.png>')
+		$('.ryu-standing').removeAttr('<img src="/images/ryu-ready-position.gif" />');	
+		$(this).attr('<img src=/images/ryu-standing-still.png>')
 
 
 	});
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".ryu-standing").prepend('<img src="/images/ryu-ready-position.gif" />'),$(".ryu-standing").click(function(){$(".ryu-standing").html("<img src=/images/ryu-standing-still.png>")})});
+$(document).ready(function(){$(".ryu-standing").attr('<img src="/images/ryu-ready-position.gif" />'),$(".ryu-standing").click(function(){$(".ryu-standing").removeAttr('<img src="/images/ryu-ready-position.gif" />'),$(this).attr("<img src=/images/ryu-standing-still.png>")})});
